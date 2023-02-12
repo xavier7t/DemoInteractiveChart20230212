6d80ae4026d881bdecdf27ec7812953a29e65392
Xavier <32859092+xavier7t@users.noreply.github.com>
2023-02-12 17:49:06 -0500

    DemoInteractiveChart20230212

--- DemoInteractiveChart20230212/ContentView.swift
+++ DemoInteractiveChart20230212/ContentView.swift
@@ -6,16 +6,59 @@
 //
 
 import SwiftUI
+import Charts
 
 struct ContentView: View {
+    @State private var selectedRecord: (String, Int)? = nil
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Chart {
+                    ForEach(salesRecords) { record in
+                        AreaMark(
+                            x: .value("Office Location", record.office),
+                            y: .value("Sales Volume", record.salesVolume)
+                        )
+                        .foregroundStyle(.linearGradient(colors: [.cyan, .cyan.opacity(0.2)], startPoint: .top, endPoint: .bottom))
+                        .foregroundStyle(.pink.opacity(0.7))
+                        if let selectedRecord {
+                            if (selectedRecord.1 > 0 && selectedRecord.1 < 5000) {
+                                RuleMark(y: .value("Sales", selectedRecord.1))
+                                    .foregroundStyle(.pink)
+                                    .annotation {
+                                        Text("\(selectedRecord.1)")
+                                            .foregroundColor(.secondary)
+                                    }
+                            }
+                        }
+                    }
+                }
+                .frame(height: 200)
+                .chartOverlay { proxy in
+                    GeometryReader { geometry in
+                        Rectangle().fill(.clear).contentShape(Rectangle())
+                            .gesture(DragGesture()
+                                .onChanged { value in
+                                    // get plot area coordiate from gesture location
+                                    let origin = geometry[proxy.plotAreaFrame].origin
+                                    let location = CGPoint(
+                                        x: value.location.x - origin.x,
+                                        y: value.location.y - origin.y)
+                                    print(location)
+                                    // update state
+                                    selectedRecord = proxy.value(at: location, as: ((String, Int).self))
+                                }
+                                .onEnded{ _ in
+                                    // reset state when gesture ends
+                                    selectedRecord = nil
+                                }
+                            )
+                    }
+                }
+                .padding()
+                .navigationTitle("Sales Report")
+            }
         }
-        .padding()
     }
 }
 
@@ -24,3 +67,20 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+//MARK: Data Source
+//Model
+struct SalesRecord: Identifiable {
+    let id = UUID()
+    let office: String
+    let salesVolume: Int
+}
+
+//Data
+let salesRecords: [SalesRecord] = [
+    .init(office: "New York", salesVolume: 3000),
+    .init(office: "San Fran", salesVolume: 2750),
+    .init(office: "California", salesVolume: 4020),
+    .init(office: "Denver", salesVolume: 930),
+    .init(office: "Kansas", salesVolume: 2100)
+]
