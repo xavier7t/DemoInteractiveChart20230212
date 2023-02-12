6f49f82d81ae3ddd9c846a2ff40d32ff1b98991d
Xavier <32859092+xavier7t@users.noreply.github.com>
2023-02-12 15:04:03 -0500

    Initial Commit

---
+++ DemoInteractiveChart20230212/DemoInteractiveChart20230212App.swift
@@ -0,0 +1,17 @@
+//
+//  DemoInteractiveChart20230212App.swift
+//  DemoInteractiveChart20230212
+//
+//  Created by Xavier on 2/12/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoInteractiveChart20230212App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
